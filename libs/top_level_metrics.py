85dd4439ce63354ace190cecbd6abf25eb93249b
GitHub Action <action@github.com>
2020-12-01 22:15:14 -0500

    7 day shift

--- libs/top_level_metrics.py
+++ libs/top_level_metrics.py
@@ -113,7 +113,7 @@ def calculate_metrics_for_timeseries(
 
 
 def calculate_fake_rt(case_density: pd.Series):
-    two_week_ratio = case_density / (case_density.shift(14) + 0.0000000001)
+    two_week_ratio = case_density / (case_density.shift(7) + 0.0000000001)
 
     return two_week_ratio.clip(lower=0, upper=3)
 
