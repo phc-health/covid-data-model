d6f10f83ed0b66b533cca39dea8dd0d013829255
czuares <corey@phc.health>
2022-06-29 13:26:09 -0400

    Pull EnvVars into CovidDataModel flow

--- generate_phc_flow.py
+++ generate_phc_flow.py
@@ -4,8 +4,9 @@ from decouple import config,Csv
 import yaml
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
-from prefect import Flow
+from prefect import Flow, task
 from prefect.tasks.shell import ShellTask
+from prefect.tasks.secrets import EnvVarSecret
 
 FLOW_STORAGE_BUCKET = config('FLOW_STORAGE_BUCKET')
 FLOW_BUCKET_PREFIX = config('FLOW_BUCKET_PREFIX', default='covid-data-model')
@@ -21,9 +22,15 @@ def get_template(template_name: str, flow_name: str) -> str:
     template_data = template_path.read_text().replace("PREFECT_FLOW_PLACEHOLDER", flow_name)
     return yaml.safe_load(template_data)
 
+@task
+def get_env_dict(keys: list) -> dict:
+    env_dict = {}
+    for k in keys:
+        env_dict[k] = EnvVarSecret(k, cast=str).run()
+    return env_dict
+
 def main():
     flow_name = "CovidDataModel"
-    shell = ShellTask()
     with Flow(
         name=flow_name,
         schedule=None,
@@ -34,7 +41,13 @@ def main():
             job_template=get_template("cdm", flow_name),
         )
     ) as f:
-        shell(command="/covid-data-model/phc.sh")
+        keys = [
+            "GCS_STORAGE_BUCKET", "GCS_BUCKET_PREFIX", "OPENBLAS_NUM_THREADS",
+            "COVID_MODEL_CORES", "DATA_OUTPUT_DIR", "UPLOAD_FILE_FILTER", "GCS_PARQUET_PATH"
+        ]
+        env_dict = get_env_dict(keys)
+        task = ShellTask(command="/covid-data-model/phc.sh", return_all=True, log_stderr=True, stream_output=True)
+        output = task(env=env_dict)
 
     f.register(project_name=PREFECT_PROJECT)
 
